acd8081029e559ad336cfc0dab3060f47936bb33
Ясюков Артём <artem@vending.by>
2025-01-22 17:34:47 +0200

    fix/final build

---
+++ Assets/scripts/OnTriggerEnter2D.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class CharacterCollision : MonoBehaviour
+{
+    // Начальная позиция для телепортации
+    public Vector3 startPosition;
+
+    // Этот метод вызывается, когда персонаж сталкивается с триггером
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        // Проверяем, что объект, с которым произошло столкновение, имеет тег "chest"
+        if (other.CompareTag("chest"))
+        {
+            // Удаляем объект, с которым произошло столкновение
+            Destroy(other.gameObject);
+        }
+
+        // Проверяем, что объект, с которым произошло столкновение, имеет тег "enemy"
+        if (other.CompareTag("enemy"))
+        {
+            // Телепортируем персонажа на стартовую позицию
+            transform.position = startPosition;
+        }
+    }
+}
