1a11d05f872348ea58efe6b51d17b0de06d1c1e4
Ясюков Артём <artem@vending.by>
2025-02-04 22:02:01 +0200

    fe/feature lvl connect

--- Assets/scripts/MainMenu.cs
+++ Assets/scripts/MainMenu.cs
@@ -5,7 +5,7 @@ public class MainMenu : MonoBehaviour
 {
     public void PlayGame()
     {
-        SceneManager.LoadScene("SampleScene");
+        SceneManager.LoadScene("JumpScene");
     }
     
     public void PlayTutorial()
