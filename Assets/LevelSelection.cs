a956e819b622fee5d473a57e1314e1906efe7695
Alina Bumbul <alina.bumbul@student.ehu.lt>
2025-02-08 12:18:44 +0200

    level_selection with auto save

---
+++ Assets/LevelSelection.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LevelSelection : MonoBehaviour
+{
+    public Button[] buttons;
+
+    private void Awake()
+    {
+        int unlockedLevel = PlayerPrefs.GetInt("UnlockedLevel", 1);
+
+        for (int i = 0; i < buttons.Length; i++)
+        {
+            if (i < unlockedLevel)
+            {
+                buttons[i].interactable = true;
+            }
+            else 
+            {
+                buttons[i].interactable = false;
+            }
+        }
+    }
+
+    public void StartLevel(int level)
+    {
+        SceneManager.LoadScene(GetLevelName(level));
+    }
+
+    protected string GetLevelName(int level)
+    {
+        switch (level)
+        {
+            case 1:
+                return "JumpScene";
+            case 2:
+                return "GravityScene";
+            case 3:
+                return "TimeScene";
+            case 4:
+                return "SampleScene";
+            default:
+                return "JumpScene";
+        }
+    }
+}
